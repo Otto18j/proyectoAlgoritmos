f0f08d6a089b8821afdc991c96a42b94bb90a9d8
ottoosuee@gmail.com <ottoj@LAPTOP-F4J3LS75>
2024-10-15 14:08:29 -0600

    primeer commit

---
+++ Gestion Inventarios/src/gestion/inventarios/GestionInventarios.java
@@ -0,0 +1,16 @@
+
+package gestion.inventarios;
+
+
+public class GestionInventarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int categoria=12;
+        System.out.println ("es doce:"+categoria);
+    }
+    
+}
